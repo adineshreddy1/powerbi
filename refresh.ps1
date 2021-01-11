9eb309e56bcbbc3433df9f8fa313d6c09f4f6bef
Dinesh Reddy <Adineshreddy1@gmail.com>
2021-01-11 23:55:02 +0530

    Update refresh.ps1
    Added Refersh

--- refresh.ps1
+++ refresh.ps1
@@ -6,6 +6,8 @@ foreach ($url in $RefreshDSURL)
 {
   #Invokes power bi url using  powerbirest method of Post and sends bodys as on notifyoption as Mailonfailure and gives verbose such that we can know from its status code.Eg 201,202
   Invoke-PowerBIRestMethod -Url $url -Method Post -Body $MailFailureNotify -Verbose
+  # specific your appromixate time limit here,so that eah url will have enough time to complete refresh
+  Start-Sleep -s 15
   #prints url into console 
   Write-Host $url
 }
