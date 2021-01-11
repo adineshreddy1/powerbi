183f0115fde82bc2432c29ac9745aa101df78915
Dinesh Reddy <Adineshreddy1@gmail.com>
2021-01-11 23:58:15 +0530

    Update refresh.ps1

--- refresh.ps1
+++ refresh.ps1
@@ -5,6 +5,7 @@ $MailFailureNotify = @{"notifyOption"="MailOnFailure"}
 foreach ($url in $RefreshDSURL)
 {
   #Invokes power bi url using  powerbirest method of Post and sends bodys as on notifyoption as Mailonfailure and gives verbose such that we can know from its status code.Eg 201,202
+  #Execute refreshes with mail on failure
   Invoke-PowerBIRestMethod -Url $url -Method Post -Body $MailFailureNotify -Verbose
   # specific your appromixate time limit here,so that eah url will have enough time to complete refresh
   Start-Sleep -s 15
